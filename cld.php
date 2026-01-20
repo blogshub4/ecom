167b6f98fdbf39f9e5068cde9854117b2167f355
blogshub4 <112010030+blogshub4@users.noreply.github.com>
2026-01-20 14:06:03 +0530

    a

---
+++ cld.php
@@ -0,0 +1,18 @@
+{
+  "$schema": "https://developer.microsoft.com/json-schemas/sp/v2/view-formatting.schema.json",
+  "calendarEventFormat": {
+    "elmType": "div",
+    "style": {
+      "background-color": "=if([$Category]=='Meeting','#0078d4', if([$Category]=='Holiday','#107c10', if([$Category]=='Training','#ff8c00', if([$Category]=='Leave','#e81123','#605e5c'))))",
+      "color": "white",
+      "padding": "6px",
+      "border-radius": "4px"
+    },
+    "children": [
+      {
+        "elmType": "span",
+        "txtContent": "@currentField"
+      }
+    ]
+  }
+}
