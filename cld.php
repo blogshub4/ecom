4497a54e8956ec14c1ab6ce34a94653cf1501073
blogshub4 <112010030+blogshub4@users.noreply.github.com>
2026-01-21 15:51:16 +0530

    Implement calendar event formatting in cld.phpd
    Added calendar event formatting with conditional styles based on category.

--- cld.php
+++ cld.php
@@ -14,6 +14,26 @@
   }
 }
 
+====
+
+{
+  "$schema": "https://developer.microsoft.com/json-schemas/sp/v2/view-formatting.schema.json",
+  "calendarEventFormat": {
+    "elmType": "div",
+    "style": {
+      "background-color": "=if([$Category]=='Meeting','#2e8b57', if([$Category]=='Training','#ffb900', if([$Category]=='Leave','#e81123','#c8c6c4')))",
+      "color": "white",
+      "padding": "4px",
+      "border-radius": "4px"
+    },
+    "children": [
+      {
+        "elmType": "span",
+        "txtContent": "@currentField"
+      }
+    ]
+  }
+}
 
 
 
